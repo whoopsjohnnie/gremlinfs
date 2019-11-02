f9bf894c6119be0aeb86bd5201a140160c9653ed
JeremyK <kuhnash@gmail.com>
2019-11-02 01:15:55 -0400

    exchanges added

--- gremlinfs.py
+++ gremlinfs.py
@@ -3837,7 +3837,7 @@ class GremlinFSOperations(Operations):
             logging.info(data)
 
             self.mq().basic_publish(
-                exchange = '',
+                exchange = 'gfs-exchange',
                 routing_key = self.config("fs_id"),
                 body = json.dumps(
                     data, 
